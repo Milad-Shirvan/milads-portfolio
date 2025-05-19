2d93397123451d0d8f992f89b154e3fe20723162
Milad-Shirvan <miladmagic3@gmail.com>
2025-05-19 18:09:44 +0200

    small fixes
    small fixes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -61,7 +61,7 @@ export default function Navbar() {
                 variant="outline"
                 className="rounded-xl border-swiss-red/30 hover:bg-swiss-red/10 h-10"
               >
-                <a href="/cv.pdf" download="Milad_Shirvan_CV.pdf">
+                <a href="/cv.pdf" download="CV.pdf">
                   CV
                 </a>
               </Button>
